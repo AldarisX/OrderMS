82433ecfddf62807870522c8885232a205a6caf9
Aldaris <z919590416@live.cn>
2018-03-16 20:25:53 +0800

    = =

--- src/main/java/com/everygamer/OrderMsApplication.java
+++ src/main/java/com/everygamer/OrderMsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.everygamer.dao"})
 @ServletComponentScan
-@MapperScan(basePackages = {"com.everygamer.dao", "com.everygamer.dao.mapping"})
 public class OrderMsApplication {
 
     public static void main(String[] args) {
