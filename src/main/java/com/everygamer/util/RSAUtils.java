d8e8b01a679838fcc68eb20ea3d3f553692c2875
Aldaris <z919590416@live.cn>
2018-03-23 09:20:06 +0800

    修改依赖

--- src/main/java/com/everygamer/util/RSAUtils.java
+++ src/main/java/com/everygamer/util/RSAUtils.java
@@ -1,18 +1,20 @@
 package com.everygamer.util;
 
-import org.apache.tomcat.util.codec.binary.Base64;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import java.security.*;
+import java.util.Base64;
+import java.util.Objects;
 
 public class RSAUtils {
-    public final static String RSAMODE = "RSA/None/PKCS1Padding";
-    public final static String RSATYPE = "BC";
-    public static PublicKey pubKey;
-    public static PrivateKey priKey;
+    private final static String RSAMODE = "RSA/None/PKCS1Padding";
+    private final static String RSATYPE = "BC";
+    private static PublicKey pubKey;
+    private static PrivateKey priKey;
     private static KeyPair keyPair;
 
     private static KeyPair initKey() {
@@ -43,7 +45,7 @@ public class RSAUtils {
      * @return
      */
     public static String decryptBase64(String string) {
-        return new String(decrypt(Base64.decodeBase64(string)));
+        return new String(Objects.requireNonNull(decrypt(Base64.getDecoder().decode(string))));
     }
 
     /**
@@ -54,21 +56,8 @@ public class RSAUtils {
      */
     public static String decryptBase64(PrivateKey priKey, String string) {
         try {
-            return new String(decrypt(priKey, Base64.decodeBase64(string)));
+            return new String(Objects.requireNonNull(decrypt(priKey, Base64.getDecoder().decode(string))));
         } catch (NoSuchProviderException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private static byte[] decrypt(PrivateKey pirKey, byte[] string) throws NoSuchProviderException {
-        try {
-            Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-            Cipher cipher = Cipher.getInstance(RSAMODE, RSATYPE);
-            cipher.init(Cipher.DECRYPT_MODE, priKey);
-            byte[] plainText = cipher.doFinal(string);
-            return plainText;
-        } catch (NoSuchAlgorithmException | InvalidKeyException | BadPaddingException | NoSuchPaddingException | IllegalBlockSizeException e) {
             e.printStackTrace();
         }
         return null;
@@ -81,17 +70,25 @@ public class RSAUtils {
      * @return
      */
     public static String encryptBase64(String string) {
-        return new String(encrypt(Base64.decodeBase64(string)));
+        return new String(Objects.requireNonNull(encrypt(Base64.getDecoder().decode(string))));
     }
 
     private static byte[] decrypt(byte[] string) {
         try {
-            Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+            return decrypt(priKey, string);
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static byte[] decrypt(PrivateKey priKey, byte[] string) throws NoSuchProviderException {
+        try {
+            Security.addProvider(new BouncyCastleProvider());
             Cipher cipher = Cipher.getInstance(RSAMODE, RSATYPE);
             cipher.init(Cipher.DECRYPT_MODE, priKey);
-            byte[] plainText = cipher.doFinal(string);
-            return plainText;
-        } catch (NoSuchAlgorithmException | InvalidKeyException | BadPaddingException | NoSuchPaddingException | NoSuchProviderException | IllegalBlockSizeException e) {
+            return cipher.doFinal(string);
+        } catch (NoSuchAlgorithmException | InvalidKeyException | BadPaddingException | NoSuchPaddingException | IllegalBlockSizeException e) {
             e.printStackTrace();
         }
         return null;
@@ -109,11 +106,19 @@ public class RSAUtils {
         return null;
     }
 
-    public static String getPubKey() {
-        return new String(Base64.encodeBase64(pubKey.getEncoded()));
+    public static String getPubKeyString() {
+        return new String(Base64.getEncoder().encode(pubKey.getEncoded()));
     }
 
-    public static String getPriKey() {
-        return new String(Base64.encodeBase64(priKey.getEncoded()));
+    public static String getPriKeyString() {
+        return new String(Base64.getEncoder().encode(priKey.getEncoded()));
+    }
+
+    public static PublicKey getPubKey() {
+        return pubKey;
+    }
+
+    public static PrivateKey getPriKey() {
+        return priKey;
     }
 }
