507da62c94e4365cf9eec4e1d3fd8570abdda7df
Aldaris <z919590416@live.cn>
2018-03-27 19:04:12 +0800

    spring-session的ddl

--- pgsql-init.sql
+++ pgsql-init.sql
@@ -143,6 +143,37 @@ CREATE TABLE admin_user_roles
     REFERENCES admin_role
 );
 
+-- session
+CREATE UNLOGGED TABLE IF NOT EXISTS spring_session
+(
+  session_id            CHAR(36) NOT NULL
+    CONSTRAINT spring_session_pk
+    PRIMARY KEY,
+  creation_time         BIGINT   NOT NULL,
+  last_access_time      BIGINT   NOT NULL,
+  max_inactive_interval INTEGER  NOT NULL,
+  principal_name        VARCHAR(100)
+);
+
+CREATE INDEX IF NOT EXISTS spring_session_ix1
+  ON spring_session (last_access_time);
+
+CREATE INDEX IF NOT EXISTS spring_session_ix2
+  ON spring_session (principal_name);
+
+CREATE UNLOGGED TABLE IF NOT EXISTS spring_session_attributes
+(
+  session_id      CHAR(36)     NOT NULL
+    CONSTRAINT spring_session_attributes_fk
+    REFERENCES spring_session
+    ON DELETE CASCADE,
+  attribute_name  VARCHAR(200) NOT NULL,
+  attribute_bytes BYTEA        NOT NULL,
+  CONSTRAINT spring_session_attributes_pk
+  PRIMARY KEY (session_id, attribute_name)
+);
+
+-- 初始admin用户 密码123456
 INSERT INTO admin_role (name) VALUES ('ROLE_ADMIN');
 INSERT INTO admin_role (name) VALUES ('ROLE_USER');
 INSERT INTO admin_user (username, password) VALUES ('admin', 'e10adc3949ba59abbe56e057f20f883e');
